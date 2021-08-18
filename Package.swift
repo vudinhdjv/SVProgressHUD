c829c12b9b5f79f9e4903b75be0cbf7036f086a5
vuDV_vn <79429357+vudinhdjv@users.noreply.github.com>
2021-08-18 10:53:39 +0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "SVProgressHUD",
-            type: .dynamic,
+            type: .static,
             targets: ["SVProgressHUD"])
     ],
     dependencies: [
